d8ad9b9df51583448787a00dc24a7db337971b47
Pavel Ustimenko <steve.jsfolio@gmail.com>
2017-06-15 00:45:50 +0200

    Add project

---
+++ Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Uc\PaymentBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('UcPaymentBundle:Default:index.html.twig');
+    }
+}
